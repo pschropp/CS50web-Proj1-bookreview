012d22b982c170eccff56da374ea397c9166da93
Pascal <pascal.schropp@dpschool.io>
2020-05-25 21:15:40 +0200

    adjusted imports; improvements on models.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,15 @@ import requests
 
 from flask import Flask, session, request
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 #from sqlalchemy import create_engine
 #from sqlalchemy.orm import scoped_session, sessionmaker
 from config import Config
-from app.models import * #own model file with classes for ORM
 
 app = Flask(__name__)
+
+from app import routes, models # routes.py and own model file with classes for ORM (has to be done after app = Flask(__name__)
 
 #importing configurations from config.py
 app.config.from_object(Config)
@@ -16,14 +19,12 @@ app.config.from_object(Config)
 #create and initiate Login-Manager for Flask-Login
 #login = LoginManager(app)
 
-# Set up database, according to imported configurations
-db.init_app(app)
-# Check for environment variable db url
+# Check for environment variable db uri
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+# Set up database and db migration, according to imported configurations
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Configure session to use filesystem, according to imported configurations
 Session(app)
-
-
-from app import routes
--- app/models.py
+++ app/models.py
@@ -1,17 +1,18 @@
 import os
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
-db = SQLAlchemy()
+from app import db
 
 class User(db.Model):
     __tablename__ = "users"
-    username = db.Column(db.String, primary_key=True)
-    useremail = db.Column(db.String, nullable=True)
-    pwdhash = db.Column(db.String, nullable=False)
-    #reviews = db.relationship("Review", backref="username", lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    useremail = db.Column(db.String(120), index=True, unique=True, nullable=True)
+    pwdhash = db.Column(db.String(128), nullable=False)
+    reviews = db.relationship("Review", backref="composer", lazy=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -22,11 +23,14 @@ class User(db.Model):
 
 class Book(db.Model):
     __tablename__ = "books"
-    isbn = db.Column(db.String, primary_key=True)
+    isbn = db.Column(db.String(13), primary_key=True)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
-    year = db.Column(db.String, nullable=False)
+    year = db.Column(db.String(4), nullable=False)
     reviews = db.relationship("Review", backref="book", lazy=True)
+
+    def __repr__(self):
+        return '<Book {}>'.format(self.isbn)
 
     def compose_review(self, username, rev_rating, rev_text):
         r = Review(isbn=self.isbn, username=username, rev_rating=rev_rating, rev_text=rev_text)
@@ -36,9 +40,11 @@ class Book(db.Model):
 
 class Review(db.Model):
     __tablename_ = "reviews"
-    rev_id = db.Column(db.String, primary_key=True)
-    isbn = db.Column(db.String, db.ForeignKey("books.isbn"), nullable=False)
-    username = db.Column(db.String, db.ForeignKey("users.username"), nullable=False)
+    rev_id = db.Column(db.String(10), primary_key=True)
+    isbn = db.Column(db.String(13), db.ForeignKey("books.isbn"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     rev_rating = db.Column(db.Integer, nullable=False)
     rev_text = db.Column(db.String, nullable=False)
 
+    def __repr__(self):
+        return '<Review {}>'.format(self.rev_text)
